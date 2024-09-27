2319dc2538515826fdb17a128d4f3bf6cca1e7fa
Pedro Bonfilio <pedroblimaa@gmail.com>
2024-09-27 19:05:22 -0300

    Fix prod build not working

--- scripts/build-app.sh
+++ scripts/build-app.sh
@@ -1,4 +1,4 @@
 export TAURI_PRIVATE_KEY="$(cat $HOME/.tauri/clipisty.key)"
 export TAURI_KEY_PASSWORD="$(cat ./tauri-private-key.txt)"
 
-npm run tauri build
+npm run tauri build -- --config src-tauri/tauri.prod.conf.json
